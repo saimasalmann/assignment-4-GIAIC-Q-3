99bd27b9e63b96c47ce7b4195c9b80c161cd190e
saimasalmann <786saimasos@gmail.com>
2025-05-06 15:24:37 +0500

    modified guess my number

--- Assignments 00 to 05/05_loops_control_flow/00_guess_my_number.py
+++ Assignments 00 to 05/05_loops_control_flow/00_guess_my_number.py
@@ -1,13 +1,24 @@
 import random
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("\033[1;31mInvalid input.\033[0m Please enter a number.")
 
-def main():
+
+
+def main(x):
+    secret_number = random.randint(1, x)
     # Generate the secret number at random!
-    secret_number = random.randint(1, 99)
     print("\n\033[1;35m****** Welcome to the Guess My Number Game ******\033[0m\n")
-    print("I am thinking of a number between 1 and 99...\n")
+    print(f"I am thinking of a number between 1 and {x}...\n")
     
     # Get user's guess
-    guess = int(input("Enter a guess: "))
+    
+    
+    
+    guess = get_int("Enter a guess: ")
     # True if guess is not equal to secret number
     while guess != secret_number:
         if guess < secret_number:  # If-statement is True if guess is less than secret number
@@ -16,9 +27,9 @@ def main():
             print("\033[1;35mYour guess is too high\033[0m")
             
         print() # Print an empty line to tidy up the console for new guesses
-        guess = int(input("Enter a new guess: "))  # Get a new guess from the user
+        guess = get_int("Enter a new guess: ") # Get a new guess from the user
         
     print("\033[1;32mCongrats! You guessed the correct number.\033[0m" )
     
 if __name__ == '__main__':
-    main()
+    main(5)
